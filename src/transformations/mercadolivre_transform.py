97f6a731dad08fd42cb23640f66543930086bcd9
juniorsilvacc <juniorsilvacc@hotmail.com>
2026-02-07 20:09:41 -0300

    feat: implementa novas colunas no dataframe: percentual_desconto, faturamento_estimado e score_oportunidade e o model estrutural de colunas que irão ser persistidas

--- src/transformations/mercadolivre_transform.py
+++ src/transformations/mercadolivre_transform.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from datetime import datetime
 from src.utils.file_handler import save_to_silver
+from src.models.mercadolivre_model import MercadoLivreProduto
 
 class MercadolivreTransform():
     def __init__(self, date_ref: str = None):
@@ -39,7 +40,7 @@ class MercadolivreTransform():
             'price_current': 'preco_atual',
             'shipping': 'envio',
             'rating': 'avaliacao',
-            'sold_raw': 'vendido'
+            'sold_raw': 'quantidade_vendida'
         })
         
         # 2. LIMPEZA E CONVERSÃO DE TIPOS (Cast)
@@ -51,16 +52,16 @@ class MercadolivreTransform():
         df['preco_atual'] = pd.to_numeric(df['preco_atual'], errors='coerce')
         df['avaliacao'] = pd.to_numeric(df['avaliacao'], errors='coerce')
         
-        if 'vendido' in df.columns:
-            df['vendido'] = df['vendido'].astype(str).str.lower().str.strip()
+        if 'quantidade_vendida' in df.columns:
+            df['quantidade_vendida'] = df['quantidade_vendida'].astype(str).str.lower().str.strip()
             # Identifica quem tem a palavra "mil" antes de removê-la. Cria uma 'máscara' listando em Verdadeiro ou Falso.
-            has_a_thousand = df['vendido'].str.contains('mil', na=False)
-            # Remove tudo que NÃO é número (limpa o "+", "mil", "vendidos", etc...)
-            df['vendido'] = df['vendido'].str.replace(r'[^\d]', '', regex=True) 
+            has_a_thousand = df['quantidade_vendida'].str.contains('mil', na=False)
+            # Remove tudo que NÃO é número (limpa o "+", "mil", "quantidade_vendidas", etc...)
+            df['quantidade_vendida'] = df['quantidade_vendida'].str.replace(r'[^\d]', '', regex=True) 
             # Multiplica por 1000 apenas onde a palavra "mil" existia
-            df['vendido'] = pd.to_numeric(df['vendido'], errors='coerce').fillna(0) 
-            df.loc[has_a_thousand, 'vendido'] = df.loc[has_a_thousand, 'vendido'] * 1000
-            df['vendido'] = df['vendido'].astype(int)
+            df['quantidade_vendida'] = pd.to_numeric(df['quantidade_vendida'], errors='coerce').fillna(0) 
+            df.loc[has_a_thousand, 'quantidade_vendida'] = df.loc[has_a_thousand, 'quantidade_vendida'] * 1000
+            df['quantidade_vendida'] = df['quantidade_vendida'].astype(int)
         
         # 3. TRATAMENTO DE VALORES NULOS (Fillna ou Dropna)
         df['loja'] = df['loja'].replace('None', 'Não Informado').fillna('Não Informado')
@@ -68,10 +69,29 @@ class MercadolivreTransform():
         df['envio'] = df['envio'].replace('None', 'Consultar Frete').fillna('Consultar Frete')
         df['avaliacao'] = df['avaliacao'].fillna(0.0)
         
-        # 4. ADICIONAR METADADOS DE CONTROLE
+        # 4. MÉTRICAS
+        # Cálculo de Desconto (Indicador de Oferta)
+        # clip(lower=0.01) para evitar divisão por zero se o preço antigo estiver bugado
+        df['percentual_desconto'] = (
+            (df['preco_antigo'] - df['preco_atual']) / df['preco_antigo'].clip(lower=0.01)
+        ).round(4)
+        
+        # Faturamento Estimado (Métrica de Volume Financeiro)
+        df['faturamento_estimado'] = (df['preco_atual'] * df['quantidade_vendida']).round(2)
+        
+        # Score de Relevância (KPI)
+        df['score_oportunidade'] = (df['avaliacao'] * df['quantidade_vendida']).round(2)
+        
+        # 5. APLICAÇÃO DO MODELO
+        # Garante que o DF tenha apenas as colunas do Model e na ordem certa
+        df = df.reindex(columns=MercadoLivreProduto.get_columns())
+        # Força os tipos de dados definidos no Model, evitando erro no Banco
+        df = df.astype(MercadoLivreProduto.get_schema_dict())
+        
+        # 6. ADICIONAR METADADOS DE CONTROLE
         df['data_processamento'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         
-        # Salvar
+        # 7. SALVAR
         save_to_silver(df, source='mercadolivre', format="parquet")
         
         return df
